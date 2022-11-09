7ebbff83562ff5546565841bd2115988f5cc2ba3
Alishia Patricio <alishia.m.patricio@gmail.com>
2022-11-08 21:08:54 -0500

    Add JenkinsFile

---
+++ JenkinsFile.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    stages {
+        stage('Create Zip File') {
+            steps {
+                script{
+                    zip dir: '', exclude: '', glob: '', zipFile: 'deploymentFile.zip'
+                } 
+            }
+        }
+        stage('Deploy Lambda Function') {
+            steps {
+                deployLambda([alias: '', artifactLocation: 'deploymentFile.zip', awsAccessKeyId: 'AKIAW27ZI5T5JZ63WEJR', awsRegion: 'us-east-1', awsSecretKey: '{AQAAABAAAAAwb8aHtRM0Uc1O4YrCnqqVyRBOKbvmCKB4nzTSEA6qJoA4Y/IZN8Rd5cnM1g7A/w5iTrHYTgnpsn9D+XUXBZYDrg==}', deadLetterQueueArn: '', description: 'Managed by Jenkins Pipeline', environmentConfiguration: [kmsArn: ''], functionName: 'HelloWorldFunction', handler: 'lambda_function.lambda_handler', memorySize: '1024', role: 'arn:aws:iam::470284823802:role/lambdaDeploymentRole', runtime: 'python3.9', securityGroups: '', subnets: '', timeout: '300', updateMode: 'full'])
+            }
+        }
+    }
+}
